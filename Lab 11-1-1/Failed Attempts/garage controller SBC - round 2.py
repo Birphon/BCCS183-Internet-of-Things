ab155610478e85187be18a1111e211d80f2b0839
Birphon <61985658+Birphon@users.noreply.github.com>
2023-05-18 22:19:42 +1200

    Idk if the new round 3 code works. Added states

--- Lab 11-1-1/Failed Attempts/garage controller SBC - round 2.py
+++ Lab 11-1-1/Failed Attempts/garage controller SBC - round 2.py
@@ -18,7 +18,7 @@ alarm_status = False
 def start():
     global door_status
     global alarm_status
-    customWrite(PIN_DOOR, DOOR_CLOSED)
+    customWrite(PIN_DOOR, DOOR_OPEN)
     door_status = "closed"
     alarm_status = False
     print("start -- door_status: " + door_status)
@@ -57,7 +57,7 @@ def close_door():
 
 
 def get_login(context, request, reply):
-    print("Get Login: " + request.url() + " @ " + request.ip())
+    print("Get_Login: " + request.url() + " @ " + request.ip())
     reply.sendFile("index.html")
     reply.setStatus(200)
     reply.end()
@@ -67,16 +67,16 @@ def post_login(context, request, reply):
     global door_status
     global alarm_status
 
-    print("Post Login: " + request.url() + " @ " + request.ip())
-    reply.setContent("posted data ...")
-    print(request.body)
-    print(type(request.body))
+    print("Post_Login: " + request.url() + " @ " + request.ip())
     if "password" in request.form:
         password = request.form["password"]
         if password == PASSWORD:
             open_door()
         else:
             close_door()
+
+    reply.setContent("posted data ...")
+    print(request.body)
     reply.setStatus(200)
     reply.end()
 
--- Lab 11-1-1/garage controller SBC - round 3.py
+++ Lab 11-1-1/garage controller SBC - round 3.py
@@ -1,7 +1,7 @@
-from realhttp import *
 from time import *
+
 from gpio import *
-from time import *
+from realhttp import *
 
 PIN_ALARM = 0
 PIN_DOOR = 1
@@ -10,11 +10,21 @@ DOOR_CLOSED = "0"
 DOOR_OPEN = "1"
 LOCAL_PORT = 8765
 PASSWORD = "12345"
-file = 'enable.html'
-password = ''
+
+ALARM_DISABLED = False
+ALARM_ENABLED = True
+DOOR_CLOSED = False
+DOOR_OPEN = True
+
+alarm_set = False
+can_door_open = False
+alarm_triggered = False
+file = "enable.html"
+password = ""
 correct_password = False
-door_status = False
-alarm_status = False
+door_status = DOOR_CLOSED
+alarm_status = ALARM_DISABLED
+
 
 def start():
     global door_status
@@ -23,27 +33,30 @@ def start():
     customWrite(PIN_DOOR, DOOR_CLOSED)
     door_status = False
     alarm_status = False
-    file = 'enable.html'
+    file = "enable.html"
     print("start -- door_status: " + door_status)
     print("start -- alarm_status: " + str(alarm_status))
 
+
 def write_lcd(msg):
     customWrite(PIN_LCD, msg)
-    
+
+
 def show_home(context, request, reply):
-    """Docstring needed here """ 
+    """Docstring needed here"""
     global password
-    
+
     request_method = request.method()
     msg_body = request.body()
     headers = request.headers()
-    
-    password = msg_body.replace('password=','')
-    print('Message body is: ' + password)
-    
+
+    password = msg_body.replace("password=", "")
+    print("Message body is: " + password)
+
     reply.sendFile(file)
     reply.setStatus(200)
     reply.end()
+
 
 def correct_pass():
     if password == PASSWORD:
@@ -51,11 +64,13 @@ def correct_pass():
     else:
         correct_password == False
 
+
 def alarm_flash():
     if alarm_flash == True:
         digitalWrite(PIN_ALARM, HIGH)
         delay(50)
         digitalWrite(PIN_ALARM, LOW)
+
 
 def open_door():
     global door_status
@@ -65,6 +80,7 @@ def open_door():
     customWrite(PIN_DOOR, DOOR_OPEN)
     print("open_door -- door_status: " + door_status)
     print("open_door -- alarm_status: " + str(alarm_status))
+
 
 def close_door():
     global door_status
@@ -80,47 +96,43 @@ def state_check():
     global door_status
     global alarm_status
     global correct_password
+    global can_door_open
+    global alarm_set
+    global alarm_triggered
     global file
 
-    # Door Closed + Alarm Enabled
-    # -> Correct Password
-    #     -> Door Open + Alarm Disabled
-    #     -> "Door: Open\nAlarm Disabled"
-    # -> Incorrect Password
-    #     -> Door Closed + Alarm Enabled
-    #     -> "Door: Closed\nAlarm Enabled"
+    if alarm_status == ALARM_DISABLED:
+        can_door_open = True
 
-    # Door Closed + Alarm Disabled
-    #     -> Alarm On
-    #     -> Correct Password
-    #         -> Door Open + Alarm Disabled
-    #         -> "Door: Open\nAlarm Disabled"
-    #     -> Incorrect Password
-    #         -> Door Closed + Alarm Enabled
-    #         -> "Door: Closed\nAlarm Enabled"
+    if door_status == DOOR_OPEN:
+        alarm_set = False
+    else:
+        alarm_set = True
 
-    # Door Open + Alarm Enabled
-    #     -> Alarms On
-    #     -> Correct Password
-    #         -> Door Open + Alarm Disabled
-    #         -> "Door: Open\nAlarm Disabled"
-    #     -> Incorrect Password
-    #         -> Door Closed + Alarm Enabled
-    #         -> "Door: Closed\nAlarm Enabled"
+    if alarm_status == ALARM_ENABLED:
+        if door_status == DOOR_OPEN:
+            alarm_flash()
+            write_lcd("Door open\nAlarm!")
+            alarm_triggered = True
 
-    # Door Open + Alarm Disabled
-    #     -> Defaults Alarm Enabled
+    if alarm_triggered == True:
+        if door_status == DOOR_CLOSED:
+            alarm_triggered = True
 
-
+    if alarm_triggered == True:
+        if correct_pass():
+            alarm_triggered = False
+            alarm_flash = False
 
 
 def main():
     server = RealHTTPServer()
-    print('Server started: '.format(server.start(8765)))
-    server.route("*", ["GET","POST"], show_home)
+    print("Server started: ".format(server.start(8765)))
+    server.route("*", ["GET", "POST"], show_home)
     while True:
         # Handle events
         sleep(1)
- 
+
+
 if __name__ == "__main__":
     main()
